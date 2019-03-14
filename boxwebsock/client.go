df3934a2ab6bc35022a362c6d1245e202238d1f3
okoshiyoshinori <catalyze.us@gmail.com>
2019-03-14 20:31:02 +0900

    update

--- boxwebsock/client.go
+++ boxwebsock/client.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	writeWait      = 10 * time.Second
+	writeWait      = 20 * time.Second
 	pongWait       = 60 * time.Second
 	pingPeriod     = (pongWait * 9) / 10
 	maxMessageSize = 1024
